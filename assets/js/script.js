1a3782cbf6fbd405a15c49d566b6f5903cb8bb3e
Paulsig007 <131915869+Paulsig007@users.noreply.github.com>
2023-05-30 20:51:45 -0600

    add script js file link to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,20 @@
-$.ajax({
-  url: "https://thecocktaildb.com/api/json/v1/1/search.php",
-  method: "GET",
-  dataType: "json",
-  success: function (response) {
-    // Handle the successful response here
-    console.log(response);
-  },
-  error: function (xhr, status, error) {
-    // Handle any errors that occur during the request
-    console.log("Error:", status, error);
-  },
-});
+// $.ajax({
+//   url: "https://thecocktaildb.com/api/json/v1/1/search.php",
+//   method: "GET",
+//   dataType: "json",
+//   success: function (response) {
+//     // Handle the successful response here
+//     console.log(response);
+//   },
+//   error: function (xhr, status, error) {
+//     // Handle any errors that occur during the request
+//     console.log("Error:", status, error);
+//   },
+// });
+
+fetch("https://thecocktaildb.com/api/json/v1/1/search.php").then((res) =>
+  console.log(res)
+);
 
 // VARIABLES
 
